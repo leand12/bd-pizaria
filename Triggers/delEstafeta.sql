6699ba609d84705628c6deb8aa3cbe65c51b2cff
Mário Silva <47184969+MarioCSilva@users.noreply.github.com>
2020-06-04 00:40:16 +0100

    finished fire courier

--- Triggers/delEstafeta.sql
+++ Triggers/delEstafeta.sql
@@ -12,15 +12,28 @@ as
         select @email = email from deleted;
 
         begin try
+			declare @ID int;
             if (exists(select top 1 email from Pizaria.Estafeta join Pizaria.Encomenda on estafeta_email=email where email=@email))
-            begin
-                update Pizaria.Estafeta set res_contato = null where email=@email
-				-- cursor para colocar o melhor estafeta em cada uma das encomendas
-                update Pizaria.Encomenda set estafeta_email = (select Pizaria.FindBestEstafeta()) where estafeta_email=@email
-                return;
-            end
+				begin
+					update Pizaria.Estafeta set res_contato = null where email=@email;
+					--cursor para colocar o melhor estafeta em cada uma das encomendas
+					DECLARE c CURSOR FAST_FORWARD
+					FOR select ID from Pizaria.Encomenda where estafeta_email=@email;
+					OPEN c;
+					FETCH NEXT FROM c into @ID;  
+
+					WHILE @@FETCH_STATUS = 0  
+					BEGIN
+						update Pizaria.Encomenda set estafeta_email = (select Pizaria.FindBestEstafeta()) where ID=@ID
+						FETCH NEXT FROM c into @ID;  
+					END;
+					CLOSE c;
+					DEALLOCATE c;
+				end
             else
-                update Pizaria.Estafeta set res_contato = null where email=@email
+				begin
+					update Pizaria.Estafeta set res_contato = null where email=@email
+				end
         end try
         begin catch
             raiserror('Error',16,1);
@@ -28,3 +41,5 @@ as
         end catch
     end
 go
+
+delete from Pizaria.Estafeta where email='a.enim@loremluctus.edu'
