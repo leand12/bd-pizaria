ddb45f7a6350d16566fc7ab3243b7aef102e96bb
leand12 <leandrosilva12@ua.pt>
2020-06-03 23:31:25 +0100

    dels

--- Triggers/delEstafeta.sql
+++ Triggers/delEstafeta.sql
@@ -5,23 +5,25 @@ go
 create trigger Pizaria.delEstafeta on Pizaria.Estafeta
 instead of delete
 as
-	begin
-		set nocount on;
-		declare @email as nvarchar(255);
+    begin
+        set nocount on;
+        declare @email as nvarchar(255);
 
-		select @email = email from deleted;
+        select @email = email from deleted;
 
-		begin try
-			if (exists(select email from Pizaria.Estafeta join Pizaria.Encomenda on estafeta_email=email where email=@email))
-			begin
-				raiserror('Error',16,1);
-				return;
-			end
-			update Pizaria.Estafeta set res_contato = null where email=@email
-		end try
-		begin catch
-			raiserror('Error',16,1);
-			return;
-		end catch
-	end
+        begin try
+            if (exists(select top 1 email from Pizaria.Estafeta join Pizaria.Encomenda on estafeta_email=email where email=@email))
+            begin
+                update Pizaria.Estafeta set res_contato = null where email=@email
+                update Pizaria.Encomenda set estafeta_email = (select Pizaria.FindBestEstafeta()) where estafeta_email=@email
+                return;
+            end
+            else
+                update Pizaria.Estafeta set res_contato = null where email=@email
+        end try
+        begin catch
+            raiserror('Error',16,1);
+            return;
+        end catch
+    end
 go
