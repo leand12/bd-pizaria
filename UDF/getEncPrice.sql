dd8e8b942589a0f88070837b2eeead43fa0dc517
leand12 <leandrosilva12@ua.pt>
2020-06-09 23:37:51 +0100

    -1 FARM PARA O CHICO, SIGAAAA

--- UDF/getEncPrice.sql
+++ UDF/getEncPrice.sql
@@ -6,17 +6,17 @@ as
 begin
 	declare @val decimal(19,2)
 	if (exists(select top 1 ID from Pizaria.Encomenda where ID = @ID))
-		set @val = (select sum(preco*quantidade*Pizaria.getDesconto(enc_ID)) as total 
+		set @val = (select sum(preco*quantidade) as total 
 				from Pizaria.Encomenda
 				join Pizaria.EncomendaItem on Encomenda.ID=enc_ID
 				join Pizaria.Item on item_ID=Item.ID
-				where enc_ID = @ID)
+				where enc_ID = @ID)*Pizaria.getDesconto(@ID)
 	else if(exists(select top 1 ID from Pizaria.EncomendaEntregue where ID = @ID))
-		set @val = (select sum(preco*quantidade*Pizaria.getDesconto(enc_ID)) as total 
+		set @val = (select sum(preco*quantidade) as total 
 			from Pizaria.EncomendaEntregue
 			join Pizaria.EncEntregueItem on EncomendaEntregue.ID=enc_ID
 			join Pizaria.Item on item_ID=Item.ID
-			where enc_ID = @ID)
+			where enc_ID = @ID)*Pizaria.getDesconto(@ID)
 	return @val
 end
 go
